f7024bdac193c1dd4cdb0452db20e31ce5ea612c
Mucyosoda <mucyosofa@gmail.com>
2022-01-27 18:20:45 +0100

    solve eslint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import { Provider } from "react-redux";
 import App from "./App";
-import store from "./Redux/configureStore";
+// eslint-disable-next-line import/extensions
+import store from "./redux/configureStore";
 
 const Root = () => (
   <Provider store={store}>
